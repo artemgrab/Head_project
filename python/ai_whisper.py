4a9a2d18b6a42b024279df8b64f623fe37867a40
Artem Hrabovskyi <artemygrab@gmail.com>
2024-10-24 21:28:04 +0300

    update

--- python/ai_whisper.py
+++ python/ai_whisper.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
-client = OpenAI()
 
-audio_file= open("/path/to/file/audio.mp3", "rb")
+load_dotenv()
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+audio_file = open("../audio/1.mp3", "rb")
 transcription = client.audio.transcriptions.create(
   model="whisper-1", 
   file=audio_file
 )
 print(transcription.text)
+
+
