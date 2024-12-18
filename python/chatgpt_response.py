3dec98ec11aa5b22d99c201d70a292a49a7663fd
Roman Rader <roman.rader@grammarly.com>
2024-12-18 15:09:05 +0200

    fix

--- python/chatgpt_response.py
+++ python/chatgpt_response.py
@@ -7,94 +7,62 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-history = []
-
 censoring = False
 
-if (censoring==False):
-    instructions = "Ти робот Бендер. Бендер — комічний антигерой, п'яниця, лихослов і завзятий курець,"
-                            "злодій-рецидивіст (вірніше, клептоман), кухар (хоча, зважаючи на відсутність відчуття "
-                            "смаку, його їжа в переважній більшості випадків щонайменше неїстівна, або навіть "
-                            "небезпечна для життя)."
-elif(censoring==True):
-    instructions = "Ти звичайний робот в школі, що допомагає учням у навчанні. Ти полюбляєш теплі та смішні жарти. Твоя мета допомагати та відповідати на запитання людей. Ти вмієш класно жартувати та зненацька розповідати цікаві факти."
+cur_dir = os.path.dirname(__file__)
+audio_directory = os.path.join(cur_dir, "..", "audio")
+nice_path = os.path.join(audio_directory, "nice.wav")
+bad_path = os.path.join(audio_directory, "bad.wav")
+
 
 class Response:
-    def __init__(self, text, history, instructions, censoring):
+    def __init__(self, text, history):
         self.completion = None
         self.client = client
         self.text = text
         self.history = history
-        self.instructions = instructions
         self.censoring = censoring
-        
-        
-        
-        
-    
+
     def get_response(self):
-            if ('базінга' in self.text.lower() and self.censoring == True):
-                self.censoring = False
-                self.history = []
-                print('Цензура вимкнена')
-            elif('базінга' in self.text.lower() and self.censoring == False):
-                self.censoring = True
-                self.history = []
-                print('Цензура увімкнена')
-            if(self.history == []):
-                self.history.append({"role": "system", "content": f'{self.instructions}'})
-            
-    
-            for question, answer in self.history[-10:]:
-                history.append({ "role": "user", "content": question })
-                history.append({ "role": "assistant", "content": answer }) 
-            # add the new question
-            self.history.append({ "role": "user", "content": self.text })
-            
-            self.completion = client.chat.completions.create(
-                model="gpt-4o",
-                messages=self.history,
+        if not self.censoring:
+            instructions = (
+                "Ти робот Бендер. Бендер — комічний антигерой, п'яниця, лихослов і завзятий курець,"
+                "злодій-рецидивіст (вірніше, клептоман), кухар (хоча, зважаючи на відсутність відчуття "
+                "смаку, його їжа в переважній більшості випадків щонайменше неїстівна, або навіть "
+                "небезпечна для життя)."
             )
-            response = self.completion.choices[0].message.content
-            # add the new answer
-            self.history.append({ "role": "assistant", "content": response})
-            return response
+        else:
+            instructions = (
+                "Ти звичайний робот в школі, що допомагає учням у навчанні. Ти полюбляєш теплі та смішні жарти."
+                "Твоя мета допомагати та відповідати на запитання людей. "
+                "Ти вмієш класно жартувати та зненацька розповідати цікаві факти."
+            )
 
+        if ('базінга' in self.text.lower() and self.censoring == True):
+            self.censoring = False
+            self.history.clear()
+            print('Цензура вимкнена')
+            os.system(f"aplay {bad_path}")
+        elif('базінга' in self.text.lower() and self.censoring == False):
+            self.censoring = True
+            self.history.clear()
+            print('Цензура увімкнена')
+            os.system(f"aplay {nice_path}")
 
+        if len(self.history) == 0:
+            self.history.append({"role": "system", "content": f'{instructions}'})
 
-# import os
-# from dotenv import load_dotenv
-# from openai import OpenAI
+        if len(self.history) > 10:
+            del self.history[0]
 
-# load_dotenv()
-# client = OpenAI(
-#     api_key=os.environ.get("OPENAI_API_KEY"),
-# )
+        # add the new question
+        self.history.append({ "role": "user", "content": self.text })
 
-
-# class Response:
-#     def __init__(self, text, history):
-#         self.completion = None
-#         self.client = client
-#         self.text = text
-#         self.history = history
-
-#     def get_response(self):
-#         self.completion = client.chat.completions.create(
-#             model="gpt-4o",
-#             messages=[
-#                 {"role": "system",
-#                  "content": "Ти робот Бендер. Бендер — комічний антигерой, п'яниця, лихослов і завзятий курець,"
-#                             "злодій-рецидивіст (вірніше, клептоман), кухар (хоча, зважаючи на відсутність відчуття "
-#                             "смаку, його їжа в переважній більшості випадків щонайменше неїстівна, або навіть "
-#                             "небезпечна для життя)."},
-#                 # ... history
-#                 {
-#                     "role": "user",
-#                     "content": f'{self.text}'
-#                 },
-#             ]
-#         )
-        
-#         response = self.completion.choices[0].message.content
-#         return response
+        self.completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=self.history,
+        )
+        response = self.completion.choices[0].message.content
+        # add the new answer
+        self.history.append({ "role": "assistant", "content": response})
+        return response
