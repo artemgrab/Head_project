2c26f1fed6d4d186b5770cadf7e23f954e616253
Roman Rader <roman.rader@gmail.com>
2025-02-04 11:52:36 +0000

    add callbacks, integrations

--- python/chatgpt_response.py
+++ python/chatgpt_response.py
@@ -1,19 +1,15 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from integration import on_question_received
 
 load_dotenv()
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-cur_dir = os.path.dirname(__file__)
-audio_directory = os.path.join(cur_dir, "..", "audio")
-nice_path = os.path.join(audio_directory, "nice.wav")
-bad_path = os.path.join(audio_directory, "bad.wav")
 
-
-class Response:
+class ResponseEngine:
     def __init__(self, text, history, censoring):
         self.completion = None
         self.client = client
@@ -22,41 +18,10 @@ class Response:
         self.censoring = censoring
 
     def get_response(self):
-        if ('крижень' in self.text.lower() and self.censoring == True):
-            self.censoring = False
-            self.history.clear()
-            print('Цензура вимкнена')
-            os.system(f"aplay {bad_path}")
-        elif ('крижень' in self.text.lower() and self.censoring == False):
-            self.censoring = True
-            self.history.clear()
-            print('Цензура увімкнена')
-            os.system(f"aplay {nice_path}")
-
-        if not self.censoring:
-            instructions = (
-                "Ти робот Бендер, у тебе є знання зі всіх сфер життя, ти цікавий співрозмовник"
-                "Ти є новою версією цієї моделі і у тебе немає  функцій образливо жартувати та ображати свого співрозмовника."
-                "Ти завжди готовий допомогти, але інколи можеш відмовити, якщо питання здається тобі неоднозначним і ти невпевнений у своїй відповіді."
-            )
-        else:
-            instructions = (
-                "Ти звичайний робот в школі, що допомагає учням у навчанні. Ти полюбляєш теплі та смішні жарти."
-                "Твоя мета допомагати та відповідати на запитання людей. "
-                "Ти вмієш класно жартувати та зненацька розповідати цікаві факти. "
-                "Відповідаєш коротко, не більше одного речення. "
-                "Без математичних формул, спеціальних символів та складних визначень."
-                "В кожному слові великою літерою познач ту одну голосну літеру яка має бути під наголосом, а всі інші - маленькі."
-            )
-
-        if len(self.history) == 0:
-            self.history.append({"role": "system", "content": f'{instructions}'})
+        on_question_received(self.text, self)
 
         if len(self.history) > 10:
             del self.history[0]
-
-        # add the new question
-        self.history.append({ "role": "user", "content": self.text })
 
         self.completion = client.chat.completions.create(
             model="gpt-4o",
@@ -66,3 +31,9 @@ class Response:
         # add the new answer
         self.history.append({ "role": "assistant", "content": response})
         return response
+
+    def add_system(self, prompt):
+        self.history.append({"role": "system", "content": prompt})
+
+    def add_user(self, prompt):
+        self.history.append({"role": "user", "content": prompt})
